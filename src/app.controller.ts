ca869e11fd0707f4b27f721e5845226255b43e55
Carsten Klein <carsten.klein@leanera.net>
2023-02-07 13:44:17 +0100

    fix raw body post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,35 +1,46 @@
-import { Body, Controller, Header, Post, StreamableFile } from '@nestjs/common';
+import { Body, Controller, Header, Post, Req, RawBodyRequest, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+
+export type RawBase64EncodedRequest<T> = T & {
+  base64Encoded?: string;
+  base64Decoded?: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('url/svg')
+  @Header('Accept', 'text/plain')
   @Header('Content-type', 'image/svg+xml')
-  async renderSvgFromUrl(@Body() body : { url: string }): Promise<string> {
-    const url = Buffer.from(body.url, 'base64').toString('utf8');
+  async renderSvgFromUrl(@Req() req: RawBase64EncodedRequest<Request>): Promise<string> {
+    console.log(req.base64Encoded);
+    console.log(req.base64Decoded);
+    const url = req.base64Decoded;
     return this.appService.renderSvgFromUrl(url).then(buffer => buffer.toString('utf8'));
   }
 
   @Post('url/png')
+  @Header('Accept', 'text/plain')
   @Header('Content-type', 'image/png')
-  async renderPngFromUrl(@Body() body : { url: string }): Promise<StreamableFile> {
-    const url = Buffer.from(body.url, 'base64').toString('utf8');
+  async renderPngFromUrl(@Req() req: RawBase64EncodedRequest<Request>): Promise<StreamableFile> {
+    const url = req.base64Decoded;
     return this.appService.renderPngFromUrl(url).then(buffer => Promise.resolve(new StreamableFile(buffer)));
   }
 
   @Post('document/svg')
+  @Header('Accept', 'text/plain')
   @Header('Content-type', 'image/svg+xml')
-  async renderSvgFromDocument(@Body() body : { content: string }): Promise<string> {
-    const document = Buffer.from(body.content, 'base64').toString('utf8');
+  async renderSvgFromDocument(@Req() req: RawBase64EncodedRequest<Request>): Promise<string> {
+    const document = req.base64Decoded;
     return this.appService.renderSvg(document).then(buffer => Promise.resolve(buffer.toString('utf8')));
   }
 
   @Post('document/png')
+  @Header('Accept', 'text/plain')
   @Header('Content-type', 'image/png')
-  async renderPngFromDocument(@Body() body : { content: string }): Promise<StreamableFile> {
-    const document = Buffer.from(body.content, 'base64').toString('utf8');
+  async renderPngFromDocument(@Req() req: RawBase64EncodedRequest<Request>): Promise<StreamableFile> {
+    const document = req.base64Decoded;
     return this.appService.renderPng(document).then(buffer => Promise.resolve(new StreamableFile(buffer)));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
+function decodeBase64EncodedString(req, _res, buffer) {
+  if (Buffer.isBuffer(buffer)) {
+    req.base64Encoded = buffer.toString();
+    req.base64Decoded = Buffer.from(req.base64Encoded, 'base64').toString('utf8');
+  }
+  return true;
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: console });
+  const app = await NestFactory.create(AppModule, { logger: console, bodyParser: false });
+  const textBodyParser = bodyParser.text({ verify: decodeBase64EncodedString });
+  app.use(textBodyParser);
   await app.listen(8080);
 }
 bootstrap();
